9c44baf5845bc49d03f7f79492ab77830abad700
Antoine Descamps <antoine.descamps@epitech.eu>
2024-03-09 23:04:11 -0500

    [+] Delivery guy and Disabled agent for vehicule
    the vehicule doesn't go straight (navmesh line blocking )

--- code/DeliveryCar.cs
+++ code/DeliveryCar.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Sandbox;
 
 
@@ -13,41 +14,68 @@ public sealed class DeliveryCar : Component
 	[Sync]
 	public bool IsReturning { get; set; }
 
-	[Category ("Parameter")]
+	[Category ("Status")]
 	[Property]
-	public GameObject Spawn { get; set; }
+	[Sync]
+	public bool IsGuyDelivering { get; set; }
+
+	[Category ("Status")]
+	[Property]
+	[Sync]
+	public bool IsWaiting { get; set; }
 
 	[Category ("Variable")]
 	[Property]
 	[Range (0.0f, 500.0f)]
 	public float DesinationMargin { get; set; } = 100.0f;
 
-	public NavMeshAgent Agent { get; set; }
+	[Category ("Preset")]
+	[Property]
+	public GameObject DeliveryGuyPrefab { get; set; }
+	public DeliveryGuy DeliveryGuy { get; set; }
+	private GameObject SpawnPositionGuy { get; set; }
+
+	// public NavMeshAgent Agent { get; set; }
 	public Vector3 Destination { get; set; }
+	public Vector3 BoxDestination { get; set; }
 
 	private SkinnedModelRenderer Renderer { get; set; }	
 	protected override void OnAwake()
 	{
 		IsDelivering = false;
-		Agent = GameObject.Components.Get<NavMeshAgent>();
+		// Agent = GameObject.Components.Get<NavMeshAgent>();
 		Renderer = Components.Get<SkinnedModelRenderer>( true );
+		SpawnPositionGuy = GameObject.Children.Where(x => x.Name == "SpawnGuy").FirstOrDefault();
 
 		Renderer.Set("Riding", true);
-		
 	}
 	protected override void OnUpdate()
 	{
 		if (IsProxy) return;
 
 		// Move the delivery car to the destination
-		if (IsDelivering || IsReturning)
-			Agent.MoveTo(Destination);
+		if ((IsDelivering && !IsWaiting) || IsReturning)
+		{
+			// Move the delivery car to the destination
+			// Agent.MoveTo(Destination);
+			Transform.Position = Transform.Position.LerpTo(Destination, 0.004f);
+		}
+
+		// if the car has arrived near the delivery destination, stop the car and spawn the delivery guy
+		if (IsDelivering && IsCarArrived() && !IsGuyDelivering)
+		{
+			StartDeliveryGuy();
+		}
+
+		// If the delivery guy has finished delivering and has arrived at the spawn, destroy the delivery guy
+		if (IsGuyDelivering && DeliveryGuy.IsReturning && IsArrived())
+		{
+			ReturnToSpawn();
+		}
 
 		// If the delivery car has finished delivering and has arrived at the spawn, destroy the delivery car
-		if (IsReturning && IsArrived())
+		if (IsReturning && IsCarArrived())
 		{
-			IsReturning = false;
-			IsDelivering = false;
 			GameObject.Destroy();
 		}
 	}
@@ -56,20 +84,64 @@ public sealed class DeliveryCar : Component
 	public void StartDelivery(Vector3 destination)
 	{
 		IsDelivering = true;
+		BoxDestination = destination;
+		destination.y = -1152.0f;
 		Destination = destination;
-		Log.Info("Delivery Car is delivering at" + Destination);
+		Log.Info("Delivery Car is delivering at: " + Destination);
 	}
 
 	// Stop the delivery car and return to the spawn
 	public void StopDelivery(Vector3 destination)
 	{
-		IsReturning = true;
 		Destination = destination;
+		DeliveryGuy.StopDelivery(SpawnPositionGuy.Transform.Position);
 	}
 
 	// Check if the delivery car has arrived at the destination
-	public bool IsArrived()
+	public bool IsCarArrived()
 	{
 		return Transform.Position.Distance(Destination) < DesinationMargin;
+	}
+	public bool IsArrived()
+	{
+		if (DeliveryGuy == null) return false;
+		return DeliveryGuy.IsArrived();
+	}
+
+	public void StartDeliveryGuy()
+	{
+		// Stop the car
+		// Agent.Stop();
+
+		// Update Status
+		IsGuyDelivering = true;
+		IsWaiting = true;
+		// Update Animation
+		Renderer.Set("Riding", true);
+		Renderer.Set("Idle", false);
+
+		// Spawn the delivery guy and start the delivery
+		var guy = DeliveryGuyPrefab.Clone();
+		guy.Transform.Position = SpawnPositionGuy.Transform.World.Position;
+		guy.NetworkSpawn();
+		DeliveryGuy = guy.Components.Get<DeliveryGuy>();
+		DeliveryGuy.StartDelivery(BoxDestination);
+	}
+
+	public void ReturnToSpawn()
+	{
+		// Update Status
+		IsReturning = true;
+		IsDelivering = false;
+		IsWaiting = false;
+		IsGuyDelivering = false;
+		
+		// Update Animation
+		Renderer.Set("Riding", false);
+		Renderer.Set("Idle", true);
+
+		// Destroy the delivery guy and rotate the car
+		DeliveryGuy.GameObject.Destroy();
+		Transform.Rotation = Rotation.From(0, 180, 0);
 	}
 }
---
+++ code/DeliveryGuy.cs
@@ -0,0 +1,90 @@
+using Sandbox;
+using Sandbox.Citizen;
+
+
+public sealed class DeliveryGuy : Component
+{
+	[Category ("Status")]
+	[Property]
+	[Sync]
+	public bool IsDelivering { get; set; }
+
+	[Category ("Status")]
+	[Property]
+	[Sync]
+	public bool IsReturning { get; set; }
+
+	[Category ("Variable")]
+	[Property]
+	[Range (0.0f, 500.0f)]
+	public float DesinationMargin { get; set; } = 10.0f;
+
+	public NavMeshAgent Agent { get; set; }
+	public Vector3 Destination { get; set; }
+
+	private CitizenAnimationHelper AnimationHelper { get; set; }	
+	protected override void OnAwake()
+	{
+		IsDelivering = false;
+		Agent = GameObject.Components.Get<NavMeshAgent>();
+		AnimationHelper = Components.Get<CitizenAnimationHelper>( true );
+	}
+
+    protected override void OnUpdate()
+    {
+        UpdateAnimation();
+    }
+	protected override void OnFixedUpdate()
+	{
+		if (IsProxy) return;
+
+		// Move the delivery car to the destination
+		if (IsDelivering || IsReturning)
+			Agent.MoveTo(Destination);
+
+		// If the delivery car has finished delivering and has arrived at the spawn, destroy the delivery car
+		if (IsReturning && IsArrived())
+		{
+			IsReturning = false;
+			IsDelivering = false;
+			GameObject.Destroy();
+		}
+	}
+
+	// Start the delivery car and move to the destination
+	public void StartDelivery(Vector3 destination)
+	{
+		IsDelivering = true;
+		Destination = destination;
+		Log.Info("Delivery Guy is delivering at: " + Destination);
+	}
+
+	// Stop the delivery car and return to the spawn
+	public void StopDelivery(Vector3 destination)
+	{
+		IsReturning = true;
+		Destination = destination;
+	}
+
+	// Check if the delivery car has arrived at the destination
+	public bool IsArrived()
+	{
+		if (Transform.Position.Distance(Destination) < DesinationMargin)
+			Log.Info(Transform.Position.Distance(Destination));
+
+		return Transform.Position.Distance(Destination) < DesinationMargin;
+	}
+
+    private void UpdateAnimation()
+	{
+		float rotateDifference = 0;
+
+		if (AnimationHelper is not null)
+		{
+			AnimationHelper.WithVelocity(110);
+			AnimationHelper.WithWishVelocity(110);
+			AnimationHelper.IsGrounded = true;
+			AnimationHelper.FootShuffle = rotateDifference;
+		}
+	}
+}
